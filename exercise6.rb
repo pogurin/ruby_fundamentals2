54dd4bc61f98d2fe1da5ffbb87c8e0287ebd2e58
MitsuyoshiToguri <seiun340066@gmail.com>
2014-10-21 15:36:10 -0400

    now I have to go to eat , I fixed exe6.

--- exercise6.rb
+++ exercise6.rb
@@ -4,3 +4,17 @@ grocery_list.each do |item|
 	puts "*#{item}" 
 end
 
+puts grocery_list.length
+if  grocery_list.include?("banana") 
+puts "You need to pick up bananas"
+else 
+	puts "You don't need to pick up bananas today"
+	end 
+	#Check to see if your list includes "bananas". If it does, output "You need to pick up bananas". Otherwise, output "You don't need to pick up bananas today".
+
+
+puts grocery_list[1]
+#Display the second item in the list.
+
+puts grocery_list.sort
+
